6f5db5de5cfc9f63293833c573b875c082b36035
Valik_Tupota <gm.tupota.valik@gmail.com>
2019-05-25 18:22:20 +0300

    frontend: added location

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 import * as VueGoogleMaps from 'vue2-google-maps'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.config.productionTip = false;
+
+
 
 Vue.use(VueGoogleMaps, {
   load: {
     key: 'AIzaSyA3RjKhvjDZUSWHeh6h4mnFhESFs9rPeBw',
     libraries: 'places', // This is required if you use the Autocomplete plugin
   },
-
 });
 
-Vue.config.productionTip = false;
+Vue.use(VueMaterial);
+
 
 
 new Vue({

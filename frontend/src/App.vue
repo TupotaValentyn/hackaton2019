b3f82e0638f8044a3cb76dd0d1b51a4bf418eea7
Valik_Tupota <gm.tupota.valik@gmail.com>
2019-05-25 12:59:33 +0300

    frontend: header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Header/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Header from './components/header.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            Header
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        height: 100vh;
+        width: 100vw;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+    }
 </style>
---
+++ frontend/src/components/header.vue
@@ -0,0 +1,45 @@
+<template>
+    <header class="header">
+        <h2 class="info">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam cum harum officiis possimus quas
+            quasi, quibusdam similique! Rerum, voluptate.
+        </h2>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+
+    }
+</script>
+
+<style>
+
+    @import url('https://fonts.googleapis.com/css?family=Maven+Pro&display=swap');
+
+    * {
+        font-family: 'Maven Pro', sans-serif;
+        box-sizing: border-box;
+    }
+
+    .header {
+        width: 100vw;
+        height: 100vh;
+        display: grid;
+        place-content: center end;
+        background-image: url('../assets/header-01.png');
+        background-size: cover;
+        padding: 0 300px 0 0;
+        /*TODO: 'need select translate for text'*/
+    }
+
+    .info {
+        width: 500px;
+        text-align: right;
+        line-height: 1.7;
+        letter-spacing: 6px;
+        text-transform: uppercase;
+    }
+
+</style>
